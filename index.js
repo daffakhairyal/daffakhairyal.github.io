cd47f14fcf0f3fc29d9b9918cbc0619e0fd4202f
daffakhairyal <117626673+daffakhairyal@users.noreply.github.com>
2024-06-21 15:11:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('nameForm');
     const resultContainer = document.getElementById('resultContainer');
-    const results = ['Mie Glosor', 'Beruang Kutub', 'Biji Kurma', 'Mang Ujang', 'Mama Ghufron', 'Harimau Jawa', 'Piso Dapur'];
+    const toggleButton = document.getElementById('toggleButton');
+    const backgroundAudio = document.getElementById('backgroundAudio');
+    const results = ['Mie Glosor', 'Beruang Kutub', 'Biji Kurma', 'Kurama', 'Kulit Pisang', 'Harimau Jawa','Kuntilanak'];
+    let isPlaying = true;
 
     form.addEventListener('submit', (event) => {
         event.preventDefault();
@@ -15,15 +18,27 @@ document.addEventListener('DOMContentLoaded', () => {
         // Create a new <p> element with the text "Khodam anda adalah"
         const resultParagraph = document.createElement('p');
         resultParagraph.textContent = "Khodam anda adalah";
-        resultParagraph.className = "text-2xl mb-2";
+        resultParagraph.className = "text-2xl mb-2 shadow-neon-red";
 
         // Create a new <h1> element with the random result
         const resultHeading = document.createElement('h1');
         resultHeading.textContent = randomResult;
-        resultHeading.className = "text-3xl text-red-600";
+        resultHeading.className = "text-3xl text-red-600 shadow-neon-red";
 
         // Append the new <p> and <h1> to the result container
         resultContainer.appendChild(resultParagraph);
         resultContainer.appendChild(resultHeading);
+    });
+
+    // Toggle button event listener
+    toggleButton.addEventListener('click', () => {
+        if (isPlaying) {
+            backgroundAudio.pause();
+            toggleButton.textContent = 'Play';
+        } else {
+            backgroundAudio.play();
+            toggleButton.textContent = 'Pause';
+        }
+        isPlaying = !isPlaying;
     });
 });
